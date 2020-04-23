4017db125a16ee8ed39236c32d14866bc1ec5cba
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-22 19:36:39 -0500

    look more like the guided project

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,53 +1,53 @@
-const enhancer = require("./enhancer.js");
+const { repair, succeed, fail } = require("./enhancer.js");
 // test away!
 
 describe("enhancer.js", () => {
   describe("repair", () => {
     it("test 100 durability", () => {
-      const itemObject = { name: "item", enhancement: 10, durability: 50 };
-      const repairObject = enhancer.repair(itemObject);
-      expect(repairObject.durability).toBe(100);
+      expect(
+        repair({ name: "item", durability: 50, enhancement: 10 }).durability
+      ).toBe(100);
     });
   });
 
   describe("succeed", () => {
     it("test increasing enhancement by 1", () => {
-      const itemObject = { name: "item", enhancement: 10, durability: 50 };
-      const succeedObject = enhancer.succeed(itemObject);
-      expect(succeedObject.enhancement).toBe(11);
+      expect(
+        succeed({ name: "item", durability: 50, enhancement: 10 }).enhancement
+      ).toBe(11);
     });
     it("test not increasing enhancement when 20", () => {
-      const itemObject = { name: "item", enhancement: 20, durability: 50 };
-      const succeedObject = enhancer.succeed(itemObject);
-      expect(succeedObject.enhancement).toBe(20);
+      expect(
+        succeed({ name: "item", enhancement: 20, durability: 50 }).enhancement
+      ).toBe(20);
     });
     it("test not changing durability", () => {
-      const itemObject = { name: "item", enhancement: 10, durability: 50 };
-      const succeedObject = enhancer.succeed(itemObject);
-      expect(succeedObject.durability).toBe(50);
+      expect(
+        succeed({ name: "item", durability: 50, enhancement: 10 }).durability
+      ).toBe(50);
     });
   });
 
   describe("fail", () => {
     it("test if item enhancement is less than 15 durability decreases by 5", () => {
-      const itemObject = { name: "item", enhancement: 10, durability: 50 };
-      const failObject = enhancer.fail(itemObject);
-      expect(failObject.durability).toBe(45);
+      expect(
+        fail({ name: "item", durability: 50, enhancement: 10 }).durability
+      ).toBe(45);
     });
     it("test if item enhancement is 15 or more durability is decreased by 10", () => {
-      const itemObject = { name: "item", enhancement: 15, durability: 50 };
-      const failObject = enhancer.fail(itemObject);
-      expect(failObject.durability).toBe(40);
+      expect(
+        fail({ name: "item", durability: 50, enhancement: 15 }).durability
+      ).toBe(40);
     });
     it("test if item enhancement is 15 or more durability is decreased by 10", () => {
-      const itemObject = { name: "item", enhancement: 16, durability: 50 };
-      const failObject = enhancer.fail(itemObject);
-      expect(failObject.durability).toBe(40);
+      expect(
+        fail({ name: "item", durability: 50, enhancement: 16 }).durability
+      ).toBe(40);
     });
     it("test if item's enhancement is greater than 16 then enhancement decreases by 1", () => {
-      const itemObject = { name: "item", enhancement: 17, durability: 50 };
-      const failObject = enhancer.fail(itemObject);
-      expect(failObject.enhancement).toBe(16);
+      expect(
+        fail({ name: "item", durability: 50, enhancement: 17 }).enhancement
+      ).toBe(16);
     });
   });
 });
